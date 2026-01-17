eceda81f5d6e77bdf12e0ddc7d9eab41bd775bc9
michael2004-source <michaelwarford2004@gmail.com>
2026-01-17 08:50:46 -0400

    fixing the intiializing

--- index.tsx
+++ index.tsx
@@ -1,17 +1,14 @@
 
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
-
-// Ensure the shimmed process is available in the module scope
-const process = (window as any).process;
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
 
-const root = ReactDOM.createRoot(rootElement);
+const root = createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <App />
